5e589bbc22694c4d63a9f18e4e3aea781fef13d7
xiaocai1247 <2198321051@qq.com>
2023-05-03 22:03:22 +0800

    test

---
+++ src/main/java/com/xiaocai/Hello.java
@@ -0,0 +1,11 @@
+package com.xiaocai;
+
+/**
+ * @author xiaocai
+ * @date 2023/5/3 21:59
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("你好，小蔡");
+    }
+}
